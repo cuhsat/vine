05e1d0bc7fb41e697e8aa89909017154587081d2
Christian Uhsat <christian@uhsat.de>
2015-03-08 01:34:21 +0100

    Updated hash

--- vine_hash.py
+++ vine_hash.py
@@ -1,1 +1,11 @@
+import sys
 
+if len(sys.argv) > 1:
+    text = sys.argv[1]
+    hash = 0
+
+    for i in range(len(text)):
+        hash = 0xFFFFFFFF & (hash << 3 | hash >> 29)
+        hash = 0xFFFFFFFF & (hash ^ ord(text[i]))
+
+    print "0x%08X" % hash
